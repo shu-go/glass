e01cf5533ef5be25b55efad3c24a5a4573a6104c
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2017-08-21 22:49:34 +0900

    change reorder functions

--- glass.go
+++ glass.go
@@ -18,44 +18,8 @@ import (
 	"github.com/urfave/cli"
 )
 
-type (
-	Rect struct {
-		Left, Top, Right, Bottom int32
-	}
-	Window struct {
-		Title       string
-		Handle      syscall.Handle
-		PID         int
-		ZPrevHandle syscall.Handle
-		Rect        Rect
-		OrgAlpha    int
-	}
-)
-
 var (
-	user32                   = syscall.NewLazyDLL("user32.dll")
-	isWindow                 = user32.NewProc("IsWindow")
-	enumWindows              = user32.NewProc("EnumWindows")
-	getWindowText            = user32.NewProc("GetWindowTextW")
-	getWindowTextLength      = user32.NewProc("GetWindowTextLengthW")
-	getWindowThreadProcessId = user32.NewProc("GetWindowThreadProcessId")
-
-	getWindow = user32.NewProc("GetWindow")
-
-	getWindowRect = user32.NewProc("GetWindowRect")
-
-	getLayeredWindowAttributes = user32.NewProc("GetLayeredWindowAttributes")
-	setLayeredWindowAttributes = user32.NewProc("SetLayeredWindowAttributes")
-	getWindowLong              = user32.NewProc("GetWindowLongW")
-	setWindowLong              = user32.NewProc("SetWindowLongW")
-	isWindowVisible            = user32.NewProc("IsWindowVisible")
-
-	kernel32 = syscall.NewLazyDLL("kernel32.dll")
-	//-> use windows.XXX
-)
-
-var (
-	verbose log.Loggerer = log.New(ioutil.Discard)
+	verbose = log.New(ioutil.Discard)
 )
 
 func initVerboseLog(c *cli.Context) {
@@ -63,109 +27,6 @@ func initVerboseLog(c *cli.Context) {
 		verbose = log.New(os.Stderr)
 	}
 	verbose.SetFlags(log.NilHeader)
-}
-
-const (
-	INFINITE     = 0xFFFFFFFF
-	SYNCHRONIZE  = 0x00100000
-	WAIT_TIMEOUT = 0x00000102
-
-	TH32CS_SNAPPROCESS = 0x00000002
-
-	WS_EX_LAYERED = 0x80000
-	LWA_COLORKEY  = 0x1
-	LWA_ALPHA     = 0x2
-	GWL_EXSTYLE   = 0xFFFFFFEC
-)
-
-func listAllWindows(allprocs bool, orgWins []*Window) (wins []*Window, err error) {
-	orgDict := makeHWND2WindowDict(orgWins)
-
-	cb := syscall.NewCallback(func(hwnd syscall.Handle, lparam uintptr) uintptr {
-		b, _, _ := isWindow.Call(uintptr(hwnd))
-		if b == 0 {
-			return 1
-		}
-
-		title := ""
-		tlen, _, _ := getWindowTextLength.Call(uintptr(hwnd))
-		if tlen != 0 {
-			tlen++
-			buff := make([]uint16, tlen)
-			getWindowText.Call(
-				uintptr(hwnd),
-				uintptr(unsafe.Pointer(&buff[0])),
-				uintptr(tlen),
-			)
-			title = syscall.UTF16ToString(buff)
-		}
-
-		prevHWND := syscall.Handle(uintptr(0))
-		result, _, _ := getWindow.Call(uintptr(hwnd), 3 /*GW_HWNDPREV*/)
-		if result != 0 {
-			prevHWND = syscall.Handle(uintptr(result))
-		}
-
-		var processID uintptr
-		getWindowThreadProcessId.Call(
-			uintptr(hwnd),
-			uintptr(unsafe.Pointer(&processID)),
-		)
-
-		r := Rect{}
-		result, _, _ = getWindowRect.Call(uintptr(hwnd), uintptr(unsafe.Pointer(&r)))
-		if result == 0 {
-			r = Rect{}
-		}
-
-		var orgWin *Window
-		if orgDict != nil {
-			if w, found := orgDict[hwnd]; found {
-				orgWin = w
-			}
-		}
-
-		var alpha uintptr
-		if orgWin != nil {
-			alpha = uintptr(orgWin.OrgAlpha)
-		} else {
-			var flag uintptr
-			result, _, _ := getLayeredWindowAttributes.Call(uintptr(hwnd), 0, uintptr(unsafe.Pointer(&alpha)), uintptr(unsafe.Pointer(&flag)))
-			if result == 0 || flag&LWA_ALPHA == 0 {
-				alpha = 255
-			}
-			/*
-				style, _, err := getWindowLong.Call(uintptr(hwnd), GWL_EXSTYLE)
-				if style&WS_EX_LAYERED != 0 {
-					result, _, _ := getLayeredWindowAttributes.Call(uintptr(hwnd), 0, uintptr(unsafe.Pointer(&alpha)), LWA_ALPHA)
-					if result == 0 {
-						alpha = 255
-					}
-				} else {
-					alpha = 255
-				}
-			*/
-		}
-
-		win := &Window{
-			Title:       title,
-			Handle:      hwnd,
-			PID:         int(processID),
-			ZPrevHandle: prevHWND,
-			Rect:        r,
-			OrgAlpha:    int(alpha),
-		}
-		wins = append(wins, win)
-
-		return 1
-	})
-
-	a, _, _ := enumWindows.Call(cb, 0)
-	if a == 0 {
-		return nil, fmt.Errorf("USER32.EnumWindows returned FALSE")
-	}
-
-	return wins, nil
 }
 
 func main() {
@@ -282,44 +143,6 @@ func main() {
 	}
 	app.Run(os.Args)
 	return
-}
-
-func filterWindowsByTitle(wins []*Window, filter string) []*Window {
-	var filters []string
-	{
-		ff := strings.Split(filter, " ")
-		filters = make([]string, 0, len(ff))
-		for _, f := range ff {
-			filters = append(filters, strings.ToUpper(f))
-		}
-	}
-
-	pid := syscall.Getpid()
-	ppid := syscall.Getppid()
-
-	var results []*Window
-	for _, w := range wins {
-		if w.PID == pid || w.PID == ppid {
-			continue
-		}
-
-		if isvisible, _, _ := isWindowVisible.Call(uintptr(w.Handle)); isvisible == 0 {
-			continue
-		}
-
-		ok := false
-		for _, f := range filters {
-			if strings.Contains(strings.ToUpper(w.Title), f) {
-				ok = true
-			}
-		}
-
-		if ok {
-			results = append(results, w)
-		}
-	}
-
-	return results
 }
 
 func runList(target string, allprocs bool) error {
@@ -460,6 +283,183 @@ wachLoop:
 	recoverAlpha(wins)
 
 	return nil
+}
+
+const (
+	INFINITE     = 0xFFFFFFFF
+	SYNCHRONIZE  = 0x00100000
+	WAIT_TIMEOUT = 0x00000102
+
+	TH32CS_SNAPPROCESS = 0x00000002
+
+	WS_EX_LAYERED = 0x80000
+	LWA_COLORKEY  = 0x1
+	LWA_ALPHA     = 0x2
+	GWL_EXSTYLE   = 0xFFFFFFEC
+)
+
+type (
+	Rect struct {
+		Left, Top, Right, Bottom int32
+	}
+	Window struct {
+		Title       string
+		Handle      syscall.Handle
+		PID         int
+		ZPrevHandle syscall.Handle
+		Rect        Rect
+		OrgAlpha    int
+	}
+)
+
+var (
+	user32                   = syscall.NewLazyDLL("user32.dll")
+	isWindow                 = user32.NewProc("IsWindow")
+	enumWindows              = user32.NewProc("EnumWindows")
+	getWindowText            = user32.NewProc("GetWindowTextW")
+	getWindowTextLength      = user32.NewProc("GetWindowTextLengthW")
+	getWindowThreadProcessId = user32.NewProc("GetWindowThreadProcessId")
+
+	getWindow = user32.NewProc("GetWindow")
+
+	getWindowRect = user32.NewProc("GetWindowRect")
+
+	getLayeredWindowAttributes = user32.NewProc("GetLayeredWindowAttributes")
+	setLayeredWindowAttributes = user32.NewProc("SetLayeredWindowAttributes")
+	getWindowLong              = user32.NewProc("GetWindowLongW")
+	setWindowLong              = user32.NewProc("SetWindowLongW")
+	isWindowVisible            = user32.NewProc("IsWindowVisible")
+
+	kernel32 = syscall.NewLazyDLL("kernel32.dll")
+	//-> use windows.XXX
+)
+
+func listAllWindows(allprocs bool, orgWins []*Window) (wins []*Window, err error) {
+	orgDict := makeHWND2WindowDict(orgWins)
+
+	cb := syscall.NewCallback(func(hwnd syscall.Handle, lparam uintptr) uintptr {
+		b, _, _ := isWindow.Call(uintptr(hwnd))
+		if b == 0 {
+			return 1
+		}
+
+		title := ""
+		tlen, _, _ := getWindowTextLength.Call(uintptr(hwnd))
+		if tlen != 0 {
+			tlen++
+			buff := make([]uint16, tlen)
+			getWindowText.Call(
+				uintptr(hwnd),
+				uintptr(unsafe.Pointer(&buff[0])),
+				uintptr(tlen),
+			)
+			title = syscall.UTF16ToString(buff)
+		}
+
+		prevHWND := syscall.Handle(uintptr(0))
+		result, _, _ := getWindow.Call(uintptr(hwnd), 3 /*GW_HWNDPREV*/)
+		if result != 0 {
+			prevHWND = syscall.Handle(uintptr(result))
+		}
+
+		var processID uintptr
+		getWindowThreadProcessId.Call(
+			uintptr(hwnd),
+			uintptr(unsafe.Pointer(&processID)),
+		)
+
+		r := Rect{}
+		result, _, _ = getWindowRect.Call(uintptr(hwnd), uintptr(unsafe.Pointer(&r)))
+		if result == 0 {
+			r = Rect{}
+		}
+
+		var orgWin *Window
+		if orgDict != nil {
+			if w, found := orgDict[hwnd]; found {
+				orgWin = w
+			}
+		}
+
+		var alpha uintptr
+		if orgWin != nil {
+			alpha = uintptr(orgWin.OrgAlpha)
+		} else {
+			var flag uintptr
+			result, _, _ := getLayeredWindowAttributes.Call(uintptr(hwnd), 0, uintptr(unsafe.Pointer(&alpha)), uintptr(unsafe.Pointer(&flag)))
+			if result == 0 || flag&LWA_ALPHA == 0 {
+				alpha = 255
+			}
+			/*
+				style, _, err := getWindowLong.Call(uintptr(hwnd), GWL_EXSTYLE)
+				if style&WS_EX_LAYERED != 0 {
+					result, _, _ := getLayeredWindowAttributes.Call(uintptr(hwnd), 0, uintptr(unsafe.Pointer(&alpha)), LWA_ALPHA)
+					if result == 0 {
+						alpha = 255
+					}
+				} else {
+					alpha = 255
+				}
+			*/
+		}
+
+		win := &Window{
+			Title:       title,
+			Handle:      hwnd,
+			PID:         int(processID),
+			ZPrevHandle: prevHWND,
+			Rect:        r,
+			OrgAlpha:    int(alpha),
+		}
+		wins = append(wins, win)
+
+		return 1
+	})
+
+	a, _, _ := enumWindows.Call(cb, 0)
+	if a == 0 {
+		return nil, fmt.Errorf("USER32.EnumWindows returned FALSE")
+	}
+
+	return wins, nil
+}
+
+func filterWindowsByTitle(wins []*Window, filter string) []*Window {
+	var filters []string
+	{
+		ff := strings.Split(filter, " ")
+		filters = make([]string, 0, len(ff))
+		for _, f := range ff {
+			filters = append(filters, strings.ToUpper(f))
+		}
+	}
+
+	pid := syscall.Getpid()
+	ppid := syscall.Getppid()
+
+	var results []*Window
+	for _, w := range wins {
+		if w.PID == pid || w.PID == ppid {
+			continue
+		}
+
+		if isvisible, _, _ := isWindowVisible.Call(uintptr(w.Handle)); isvisible == 0 {
+			continue
+		}
+
+		ok := false
+		for _, f := range filters {
+			if strings.Contains(strings.ToUpper(w.Title), f) {
+				ok = true
+			}
+		}
+
+		if ok {
+			results = append(results, w)
+		}
+	}
+
+	return results
 }
 
 type WinNode struct {
