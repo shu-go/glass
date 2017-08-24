9037107e7ca79af9ea47c3c38c452078df1df127
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2017-08-24 23:47:25 +0900

    change much boost alpha of background windows

--- glass.go
+++ glass.go
@@ -621,5 +621,5 @@ func recoverAlpha(wins []*Window) {
 }
 
 func alphaFromPercent(percent, level int) uintptr {
-	return uintptr(255 * math.Pow(float64(100-percent)/100, math.Pow(float64(level), 2)))
+	return uintptr(255 * math.Pow(float64(100-percent)/100, math.Pow(float64(level), 3)))
 }
