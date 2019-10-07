279ba25ea74d4e4832f1799c1086b6d3fe6edfc7
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2019-10-07 14:16:48 +0900

    add sample.bat

---
+++ sample.bat
@@ -0,0 +1,12 @@
+@echo off
+set DEFAULT=youtube ニコニコ ビデオ vlc
+
+set /p TGT=対象(省略時は、%DEFAULT%): 
+if "%TGT%" == "" (
+    set TGT=%DEFAULT%
+)
+
+glass.exe watch %TGT% -a 30 -i 100ms %*
+
+@echo on
+exit /b 0
